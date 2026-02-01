08b9b809caccab90b8e5b796a44d9cb2c8aa884c
megacoder001 <aatrox_zhmayq@mail.ru>
2026-02-01 13:46:09 +0300

    01.01.02 (JS)

--- games/tictactoegame/state.js
+++ games/tictactoegame/state.js
@@ -1,6 +1,24 @@
+const CellState {
+	EMPTY: 0,
+	CROSS: 1,
+	CIRCLE: 2
+};
+
+const TurnState {
+	CROSS: 1,
+	CIRCLE: 2,
+};
+
+class TicTacToeState {
+	const cellStates = Array(9).fill(CellState.EMPTY);
+	let turnState = TurnState.CROSS;
+};
+
+
+/*
 const container = document.getElementById('imageContainer');
 const overlayImage = document.getElementById('overlayImage');
 
 container.addEventListener('click', () => {
     overlayImage.style.display = 'block'; // Show image02.png inside image01.jpg
-});
+}); */
