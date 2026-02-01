56adc7e1c37fc5040faa3c95b026e7eef270d11c
megacoder001 <aatrox_zhmayq@mail.ru>
2026-02-01 22:37:21 +0300

    01.01.05 (mini-fixes)

--- games/tictactoegame/main.js
+++ games/tictactoegame/main.js
@@ -5,7 +5,7 @@ let boardState = new TicTacToeState();
 
 let boardRenderer = new Renderer(boardState);
 
-boardRenderer.render();
+// boardRenderer.render();
 boardState.cellStates[2] = CellState.CROSS;
 boardState.cellStates[5] = CellState.CIRCLE;
 boardState.cellStates[8] = CellState.CROSS;
