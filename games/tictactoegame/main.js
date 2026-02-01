7c6bb53e2dd6a987efb0b5f5f15ccbd79c5d5375
megacoder001 <aatrox_zhmayq@mail.ru>
2026-02-01 16:14:11 +0300

    01.01.04.03 (mini-fix)

--- games/tictactoegame/main.js
+++ games/tictactoegame/main.js
@@ -4,3 +4,9 @@ import {Renderer} from './renderer.js';
 let boardState = new TicTacToeState();
 
 let boardRenderer = new Renderer(boardState);
+
+boardRenderer.render();
+boardState.cellStates[2] = CellState.CROSS;
+boardState.cellStates[5] = CellState.CIRCLE;
+boardState.cellSTATES[8] = CellState.CROSS;
+boardRenderer.render();
