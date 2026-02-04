ad39949f2667ed22c1f68a6e330135d3fe3396ec
megacoder001 <aatrox_zhmayq@mail.ru>
2026-02-04 20:38:01 +0300

    01.01.06 (mini-fixes)

--- games/tictactoegame/renderer.js
+++ games/tictactoegame/renderer.js
@@ -1,13 +1,10 @@
 import {TicTacToeState, CellState} from './state.js';
 // import {img} from './imageprovider.js';
 
-const canvas = document.getElementById('myCanvas');
-const ctx = canvas.getContext('2d');
-const backgroundImage = new Image();
-const backgroundImage.src = '../../images/gamesimages/tictactoeimages/tictactoefield.jpg';
+// const canvas = document.getElementById('myCanvas');
+// const ctx = canvas.getContext('2d');
 
-// const overlayImage
-// doesn't work: img.src = '../../images\gamesimages\tictactoeimages\tictactoeprocessingimages/redcross01.png';
+// const overlayImage doesn't work: img.src = '../../images\gamesimages\tictactoeimages\tictactoeprocessingimages/redcross01.png';
 
 export class Renderer {
 	constructor(boardState) {
@@ -25,8 +22,12 @@ export class Renderer {
 
 			else if (value == CellState.CROSS) {
 				console.log('x');
-				backgroundimg.onload = () => {
-					ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height); // Draw background image
+				// backgroundimg.onload = () => {
+				//	ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height); // Draw background image
+				let backgroundImage = document.createElement('img');
+				backgroundImage.src = '../../images/gamesimages/tictactoeimages/tictactoefield.jpg';
+				document.body.appendChild(img);
+
 				});
 
 			}
