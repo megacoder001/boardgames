0f7d528d730dfa0ae517be82307333b294b5fc47
megacoder001 <aatrox_zhmayq@mail.ru>
2026-02-04 18:09:56 +0300

    01.01.06 (mini-fixes)

--- games/tictactoegame/renderer.js
+++ games/tictactoegame/renderer.js
@@ -1,6 +1,11 @@
 import {TicTacToeState, CellState} from './state.js';
 // import {img} from './imageprovider.js';
 
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext('2d');
+img = new Image();
+img.src = '../../../images/redcross01.png';
+
 export class Renderer {
 	constructor(boardState) {
 		// boardState must be TicTacToeState
@@ -17,10 +22,6 @@ export class Renderer {
 
 			else if (value == CellState.CROSS) {
 				console.log('x');
-				canvas = document.getElementById('myCanvas');
-				ctx = canvas.getContext('2d');
-				img = new Image();
-				img.src = '../../../images/redcross01.png';
 				img.addEventListener('load', () => {
 					ctx.drawImage(img, 0, 0); // Draw the image at coordinates (0, 0)
 				});
