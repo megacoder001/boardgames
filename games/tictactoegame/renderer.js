6214f5a7d7190343ee4c8eaeb62e08a59a92e350
megacoder001 <aatrox_zhmayq@mail.ru>
2026-02-04 18:57:25 +0300

    01.01.06 (mini-fixes)

--- games/tictactoegame/renderer.js
+++ games/tictactoegame/renderer.js
@@ -22,7 +22,7 @@ export class Renderer {
 
 			else if (value == CellState.CROSS) {
 				console.log('x');
-				img.addEventListener('load', () => {
+				img.addEventListener('load', ('../../images\gamesimages\tictactoeimages\tictactoeprocessingimages/redcross01.png'') => {
 					ctx.drawImage(img, 0, 0); // Draw the image at coordinates (0, 0)
 				});
 
