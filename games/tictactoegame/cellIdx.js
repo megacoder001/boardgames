58674b0f7d680f70e0d1d24eb3bc5a4e564794fc
megacoder001 <aatrox_zhmayq@mail.ru>
2026-02-12 21:51:35 +0300

    01.02.04.04 (RIGHT Workable version with coordinates)

--- games/tictactoegame/cellIdx.js
+++ games/tictactoegame/cellIdx.js
@@ -1,12 +1,12 @@
 export class cellDetection {
-        cellDetectionFunction(clickArea) {
-                clickArea.addEventListener('click', function(event) {
-                        var x = event.clientX;
-                        var y = event.clientY;
-                        console.log('X: ' + x + ', Y: ' + y);
-                        return x, y;
-                })
-        }
+	cellDetectionFunction(clickArea) {
+		clickArea.addEventListener('click', function(event) {
+			var x = event.clientX;
+			var y = event.clientY;
+			console.log('X: ' + x + ', Y: ' + y);
+			return x, y;
+		})
+	}
 };
 
 // everything's working, but there's one problem: AI wrote that clickArea must be "var", but it is "const" and works
--- games/tictactoegame/main.js
+++ games/tictactoegame/main.js
@@ -1,37 +1,25 @@
 import {TicTacToeState, CellState} from './state.js';
 import {Drawer} from './drawer.js';
 import {loadImage} from './imageloader.js';
-// import {cellDetection} from './cellIdx.js';
+import {cellDetection} from './cellIdx.js';
 
+//
 let boardState = new TicTacToeState();
+
+// 
 let boardDrawer = new Drawer(boardState);
 
-// for cellIdx.js
-export class cellDetection {
-        cellDetectionFunction(clickArea) {
-                clickArea.addEventListener('click', function(event) {
-                        var x = event.clientX;
-                        var y = event.clientY;
-                        console.log('X: ' + x + ', Y: ' + y);
-                        return x, y;
-                })
-        }
-};
-
-
-const clickArea = document.getElementById('canvasIdxDetection');
+// Getting coordinates
 let clickIdx = new cellDetection();
+var clickArea = document.getElementById('canvasIdxDetection');
 
-// var clickArea = document.getElementById('canvasIdxDetection');
-// const clickArea = document.getElementById('canvasIdxDetection')
-
-
+//
 boardState.nextTurn(clickIdx.cellDetectionFunction(clickArea));
 boardState.nextTurn(2);
 boardState.nextTurn(5);
 boardState.nextTurn(7);
 
-
+// Getting element's id
 const canvas = document.getElementById('boardCanvas');
 const canvasContext2d = canvas.getContext('2d');
 
