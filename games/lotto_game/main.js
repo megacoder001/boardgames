8ee0b1907de48de6398a5e1d7540f8685eb2d1c5
megacoder001 <aatrox_zhmayq@mail.ru>
2026-03-07 21:00:16 +0300

    02.02 (Workable)

--- games/lotto_game/main.js
+++ games/lotto_game/main.js
@@ -15,10 +15,14 @@ const [ticket] = await Promise.all (
 
 const nextBarrel = document.getElementById('next_barrel');
 
-for (letplayer = 0; player < 2; player++) {
+for (let player = 0; player < 2; player++) {
+	const currentPlayerTickets = document.getElementById('player-tickets-' + player);
+
+
 	for (let i = 0; i < 3; i++) {
 		const ticketCanvas = document.createElement('canvas');
-		document.body.insertBefore(ticketCanvas, nextBarrel);
+//		document.body.insertBefore(ticketCanvas, nextBarrel);
+		currentPlayerTickets.appendChild(ticketCanvas);
 		ticketCanvas.className = 'ticket-Canvas';
 		ticketCanvas.getContext('2d').drawImage(ticket, 0, 0, ticketCanvas.width, ticketCanvas.height);
 	}
