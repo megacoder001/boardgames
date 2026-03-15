5446454e44e8e7f8078bda26cd556b7933bb4b36
megacoder001 <aatrox_zhmayq@mail.ru>
2026-03-15 12:31:05 +0300

    02.04 Workable

--- games/lotto_game/ran_num.js
+++ games/lotto_game/ran_num.js
@@ -1,9 +1,10 @@
-export function getBarNum() {
-	const unusedNums = {};
-	for (let ind = 1; ind <= 90; ind++) {
-		unusedNums[ind] = true;
-	}
+// refactor to class if you want to use it from several places
+const unusedNums = {};
+for (let ind = 1; ind <= 90; ind++) {
+	unusedNums[ind] = true;
+}
 
+export function getBarNum() {
 	const keys = Object.keys(unusedNums);
 	const barNum = keys[Math.floor(Math.random() * keys.length)];
 
